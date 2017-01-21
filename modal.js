3b8050079b1637f4deb977be8e3d1209a60eefd6
Mark <mark@automaticclients.com>
2017-01-21 12:34:15 -0800

    Changed color scheme, but wait, There's more... less

---
+++ modal.js
@@ -0,0 +1,12 @@
+
+
+var $ = jQuery;
+
+$(document).ready(function(){
+   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+   $('.modal').modal();
+ });
+
+$('#modal1').modal('open');
+
+$('#modal1').modal('close');
