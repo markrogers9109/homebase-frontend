ef02ca514f7af8edc79368f4c032bc25ce934e5a
Mark <mark@automaticclients.com>
2017-01-27 18:26:57 -0800

    added sub-menu and sub-menu trasition fuctionality

--- js/nav-mobile.js
+++ js/nav-mobile.js
@@ -1,6 +1,30 @@
 
 var $ = jQuery;
 
-$(document).ready(function(){
-    $('.collapsible').collapsible();
+$('.toggle-subnav').click(function(){
+    if ($('.fixed-navbar').hasClass('show-submenu')) {
+        $('.fixed-navbar').removeClass('show-submenu');
+        $('.sub-nav_column').removeClass('toggle-subnav');
+        $('.toggle-subnav_top').removeClass('toggle-subnav');
+        $('.toggle-subnav_top').removeClass('activate');
+    } else {
+        $('.fixed-navbar').addClass('show-submenu');
+        $('.sub-nav_column').addClass('toggle-subnav');
+        $('.toggle-subnav_top').addClass('toggle-subnav');
+        $('.toggle-subnav_top').addClass('activate');
+    }
   });
+
+  $('.toggle-subnav_top').click(function(){
+      if ($('.fixed-navbar').hasClass('show-submenu')) {
+          $('.fixed-navbar').removeClass('show-submenu');
+          $('.sub-nav_column').removeClass('toggle-subnav');
+          $('.toggle-subnav_top').removeClass('toggle-subnav');
+          $('.toggle-subnav_top').removeClass('activate');
+      } else {
+          $('.fixed-navbar').addClass('show-submenu');
+          $('.sub-nav_column').addClass('toggle-subnav');
+          $('.toggle-subnav_top').addClass('toggle-subnav');
+          $('.toggle-subnav_top').addClass('activate');
+      }
+    });
