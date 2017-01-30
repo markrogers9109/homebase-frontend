435d7c27ebf3c11f4c9dadbba838bf855dc13c7c
Mark <mark@automaticclients.com>
2017-01-30 15:26:21 -0800

    Started organizing to be converted to full app

---
+++ app/images/carousel.js
@@ -0,0 +1,16 @@
+
+var $ = jQuery;
+
+$(document).ready(function(){
+      $('.carousel').carousel();
+    });
+
+    $('.carousel').carousel('next');
+    $('.carousel').carousel('next', 3); // Move next n times.
+    // Previous slide
+    $('.carousel').carousel('prev');
+    $('.carousel').carousel('prev', 4); // Move prev n times.
+    // Set to nth slide
+    $('.carousel').carousel('set', 4);
+
+$('.carousel.carousel-slider').carousel({full_width: true});
---
+++ app/js/nav.js
@@ -0,0 +1,20 @@
+
+var $ = jQuery;
+
+$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+
+$('.btn-floating').click( function() {
+    if ($('.add-icon').hasClass('open-scroll')) {
+        $('.add-icon').removeClass('open-scroll');
+        $('.activity-overflow').removeClass('activity-scroll');
+    } else {
+        $('.add-icon').addClass('open-scroll');
+        $('.activity-overflow').addClass('activity-scroll');
+    }
+});
